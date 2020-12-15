48f3fb94f89bdf4a89308caf37ebf8eb72dffce7
Kimberly <kmglock@mail.lipscomb.edu>
2020-12-14 19:10:34 -0600

    commit tree 415b73557922619e66426ba28f38c336eba56fc3

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  arr = [] # create empty array
+  array.each { |i| arr << i ** 2 } # i is the block variable # << pushes new value into arr array # braces are another way to start and end block instead of do/end
+  arr # final line in method block gets returned:) 
+    #new_array.unshift(element ** 2)
+    #return new_array.unshift(element**2)
+  #end # no need for this because we used {}. do would need an end though.
 end
